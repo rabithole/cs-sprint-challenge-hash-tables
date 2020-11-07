4935f0c0053253fd61cfb21dd4a8dec1c4993f30
Robert Kulp <pedalinon@gmail.com>
2020-11-06 20:25:52 -0800

    progress

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,13 +4,22 @@ packages = [12, 6, 7, 14, 19, 3, 0, 25, 40]
 def get_indices_of_item_weights(weights, length, limit):
 
     dict = {}
+    dict1 = {}
     
     for i in range(0, length):
     	temp = limit - weights[i]
     	    	
     	dict[weights[i]] = temp
+    	for weight in dict:
+    		iter = dict[weight]
+    		# print('Value', dict[weight], 'key', weight)
+    		# dict[weight] = value or iter
+    		# weight = key
+    		if iter in dict:
+    			print('sum equals', dict[weight], weight)
 
-    print('Printing dict -------', dict[12])
+
+    print('Printing dict -------', dict)
 
     return None
 
