b1ef470c742bd8d631219fa6b565cee60e2a6729
Robert Kulp <pedalinon@gmail.com>
2020-11-07 15:18:18 -0800

    Almost MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,25 @@
+theList = [ 1, -1, 2, 3, -4, -3, 4, -5, 6, 7 ]
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+    dict = {}
+    result = []
 
+    for num in a:
+    	# print('Num', num)
+    	if abs(num) not in dict:
+    		dict[abs(num)] = abs(num)
+    		# print('Dict', dict)
+    	else:
+    		result.append(abs(num))
+    		# print('Result', result)
+    # print('Result', result)
     return result
+
+# has_negatives(theList)
 
 
 if __name__ == "__main__":
