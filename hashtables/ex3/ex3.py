a3a442c868b1fb9d65501aa01ddfbd82353c57ab
Robert Kulp <pedalinon@gmail.com>
2020-11-07 14:49:01 -0800

    Two done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,35 @@
+listList = [
+    [1, 2, 3, 4, 5],
+    [12, 7, 2, 9, 1],
+    [99, 2, 7, 1,]
+]
+
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
     # Your code here
+    dict = {}
+    result = []
 
+    for array in arrays:
+        for element in array:
+            if element not in dict:
+                dict[element] = 1
+                # print(dict[element])
+            else: 
+                dict[element] += 1
+                # print(dict[element])
+    # print(dict)
+    for key in dict:
+        # print('Dict key', dict[key], 'Array length', len(arrays))
+        if dict[key] == len(arrays):
+            result.append(key)
+
+    print(result)
     return result
+
+# intersection(listList)
 
 
 if __name__ == "__main__":
