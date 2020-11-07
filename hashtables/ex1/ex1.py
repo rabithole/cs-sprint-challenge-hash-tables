934099b7d834da02c4b12a34f49f1c4003c73e27
Robert Kulp <pedalinon@gmail.com>
2020-11-06 19:41:45 -0800

    I need a break

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,20 @@
+packages = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+
+
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    dict = {}
+    
+    for i in range(0, length):
+    	temp = limit - weights[i]
+    	    	
+    	dict[weights[i]] = temp
+
+    print('Printing dict -------', dict[12])
 
     return None
+
+get_indices_of_item_weights(packages, len(packages), 22)
+
+
+
