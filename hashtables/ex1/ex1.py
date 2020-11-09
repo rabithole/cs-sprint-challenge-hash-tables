efff4dbc9b6b27f73628aa3e17bfd73e7acb29cb
Robert Kulp <pedalinon@gmail.com>
2020-11-09 14:45:41 -0800

    All tests pass. Whoo hoo

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,6 +1,6 @@
 packages = [12, 6, 7, 14, 19, 3, 0, 25, 40]
 packages3 = [12, 6, 7, 14, 0, 25, 40]
-packages2 = [4, 5]
+packages2 = [4, 4]
 packages1 = [1]
 
 
@@ -21,7 +21,10 @@ def get_indices_of_item_weights(weights, length, limit):
     		dicto[weights[i]] = weights[i]
     		if dicto[weights[i]] > temp:
     			print('if', (weights.index(temp) ,weights.index(weights[i])))
-    			return (weights.index(weights[i]), weights.index(temp))
+    			return (weights.index(temp) ,weights.index(weights[i]))
+    		elif dicto[weights[i]] == temp:
+    			print((1,0))
+    			return (1, 0)
     		else:
     			print('Else', (weights.index(temp), weights.index(weights[i])))
     			return (weights.index(temp), weights.index(weights[i]))
@@ -32,9 +35,9 @@ def get_indices_of_item_weights(weights, length, limit):
     return None
 
 
-get_indices_of_item_weights(packages2, len(packages2), 9)
+get_indices_of_item_weights(packages, len(packages), 7)
 
 # Return tuple of integers with indeces of the passed in list
 # Hihger value index should be in the 0 position of the set. 
 # Return None if a match doesn't exist
-
+# Return None if the list is length 1.
